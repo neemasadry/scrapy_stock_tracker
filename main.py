ad1a749a3af5795f9da78a6cdeb9d60c344268f5
Neema Sadry <nsadry@influxdata.com>
2022-07-15 13:45:00 -0400

    Added InfluxDB write function to pipelines.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import schedule
-import time
-import os
+import schedule, time, os
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 print('Scheduler initialised')
-schedule.every(10).seconds.do(lambda: os.system('scrapy crawl yfinance -o yfinance.csv -t csv'))
+schedule.every(30).seconds.do(lambda: os.system('scrapy crawl yfinance -o yfinance.csv -t csv'))
 print('Next job is set to run at: ' + str(schedule.next_run()))
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+
--- stock_tracker/pipelines.py
+++ stock_tracker/pipelines.py
@@ -5,6 +5,12 @@
 
 
 # useful for handling different item types with a single interface
+
+# send_data.py - InfluxDB Intern Hackathon
+# File containing functions to: 
+# 1. parse a CSV file that contains stock tickers, prices, sources, and dates
+# 2. send organized data to a bucket in InfluxDB
+
 import scrapy, os, sys
 from datetime import datetime
 from mimetypes import guess_extension
@@ -20,26 +26,25 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 from influxdb_client.client.write_api import ASYNCHRONOUS
 
 # You can generate an API token from the "API Tokens Tab" in the UI
-token = "e_se0YBgpHmSSe4Uj-AzFVQ3U8ImnzD0J_4tE50x0e61ruWP4BUuiyhHCsanC1mjpQ09iMvdn6CxsmvvVKedkA=="
-org = "Hackathon Team"
-bucket = "stock_tracker"
-influxdb_url = "http://localhost:8086"
-
+bucket = "stocks_recent"
+org = "9c5955fc99a60b8f"
+token = "Z1zTLcdabiA3tXhxQ3vHqt5_UVy0TJLrYVqdpomCr8FnYCGKJMKmQfeNOgcISV2qoNJkYGvuWm2c-gsWpj41tw=="
 
 
 class StockTrackerPipeline:
-    def __init__(self):
-        client = influxdb_client.InfluxDBClient(
-            url=influxdb_url,
-            token=token,
-            org=org
-        )
 
     def process_item(self, item, spider):
-        write_api = client.write_api(write_options=SYNCHRONOUS)
-
-        data = "mem,host=host1 used_percent=23.43234543"
-        write_api.write(bucket, org, data)
-        return item
-
-        
+        with InfluxDBClient(url="https://twodotoh-dev-andrew20220517115401.remocal.influxdev.co", token=token, org=org) as client:
+            write_api = client.write_api(write_options=SYNCHRONOUS)
+            
+            with open('yfinance.csv', 'r') as f:          # Read lines separately
+                reader = csv.reader(f, delimiter=',')
+                for line in reader:
+                    if line[0] == "company_name": continue
+                
+                    p = Point("Stocks") \
+                        .tag("Ticker", "Ticker") \
+                        .field(line[3], float(line[2])) \
+                        .time(line[1])
+                    
+                    write_api.write(bucket, org, p)
